d8bf486e541452983b25a546f9ccb47b4b29bf61
Cacho <daniel_cacho@hotmail.com>
2025-05-30 15:42:39 -0600

    fixed index.js

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173', 
-    'https://ags-ffl-jyev.vercel.app'  // ← Sin barra final
+    'https://ags-ffl-jyev.vercel.app'
   ],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Content-Disposition'],
